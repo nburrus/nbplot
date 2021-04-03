b2d74c329f4fe0d2be7067a3cc5509c8f13a5575
Nicolas Burrus <nb@arcturus.industries>
2021-04-03 16:08:48 +0200

    Add a setup.py to enable pip install -e.
    Bump the version number.

--- nbplot/__init__.py
+++ nbplot/__init__.py
@@ -1,3 +1,3 @@
 """Empty init.py to make it a package"""
-__version__ = '0.1'
+__version__ = '0.2'
 from .nbplot import main
---
+++ setup.py
@@ -0,0 +1,4 @@
+# Everything is defined in setup.cfg, added this file only
+# to support editable mode.
+import setuptools
+setuptools.setup()
